a5c3d194a04ac5a2f2d483df7daa6871970266b4
Faddah Wolf <my_biz@me.com>
2025-02-05 09:33:43 -0800

    Re-factored import @tailwindcss/vite to @tailwindcss/postcss, per Vinny.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import cloudflare from '@astrojs/cloudflare';
 import node from '@astrojs/node';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/postcss';
 
 // https://astro.build/config
 export default defineConfig({
